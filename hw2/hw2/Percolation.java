117933ede3f595c38461c1bd00ea37181d76931d
gavingoh99 <gavingoh99@gmail.com>
2021-11-24 11:22:06 +0800

    edited percolationstats

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,21 +3,23 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private int[][] array;
+    private boolean[][] array;
     private int sites;
     private int topIndex;
     private int bottomIndex;
     private WeightedQuickUnionUF disjoint;
+    private WeightedQuickUnionUF disjointTopOnly;
     public Percolation(int N) {
         if (N <= 0) {
             throw new IllegalArgumentException();
         }
-        array = new int[N][N];
+        array = new boolean[N][N];
         sites = 0;
         // create a disjoint with N * N capacity,
         // we'll store the coordinates as a single int variable
         // from 0 to (N * N - 1)
         disjoint = new WeightedQuickUnionUF(N * N + 2);
+        disjointTopOnly = new WeightedQuickUnionUF(N * N + 1);
         // implement a virtual site that exists
         // at the top and bottom of the grid
         // to connect to sites at top and bottom
@@ -26,7 +28,7 @@ public class Percolation {
         // fill the array with 0 to denote not open
         for (int row = 0; row < N; row++) {
             for (int col = 0; col < N; col++) {
-                array[row][col] = 0;
+                array[row][col] = false;
             }
         }
     }
@@ -36,33 +38,42 @@ public class Percolation {
             throw new ArrayIndexOutOfBoundsException();
         }
         if (!isOpen(row, col)) {
-            array[row][col] = 1;
+            array[row][col] = true;
             sites++;
         }
-
+        // we make use of two quickunions to handle the backwash
+        // one quickunion only has a top virtual site
+        // we'll check for connection using this quickunion
+        // while we check for connection using the other
+        // for percolation
         int setIndex = getSetIndex(row, col);
         if (row - 1 >= 0 && isOpen(row - 1, col)) {
             disjoint.union(setIndex, setIndex - array.length);
+            disjointTopOnly.union(setIndex, setIndex - array.length);
         } else if (row == 0) {
             disjoint.union(topIndex, setIndex);
+            disjointTopOnly.union(topIndex, setIndex);
         }
         if (row + 1 < array.length && isOpen(row + 1, col)) {
             disjoint.union(setIndex, setIndex + array.length);
+            disjointTopOnly.union(setIndex, setIndex + array.length);
         } else if (row == array.length - 1) {
             disjoint.union(bottomIndex, setIndex);
         }
         if (col + 1 < array.length && isOpen(row, col + 1)) {
             disjoint.union(setIndex, setIndex + 1);
+            disjointTopOnly.union(setIndex, setIndex + 1);
         }
         if (col - 1 >= 0 && isOpen(row, col - 1)) {
             disjoint.union(setIndex, setIndex - 1);
+            disjointTopOnly.union(setIndex, setIndex - 1);
         }
     }
     public boolean isOpen(int row, int col) {
         if (row >= array.length || col >= array.length) {
             throw new ArrayIndexOutOfBoundsException();
         }
-        return array[row][col] == 1;
+        return array[row][col];
     }
     // there is a drawback of virtual sites
     // if we are not discriminate in connecting them,
@@ -81,7 +92,7 @@ public class Percolation {
             throw new ArrayIndexOutOfBoundsException();
         }
         int setIndex = getSetIndex(row, col);
-        return disjoint.connected(setIndex, topIndex);
+        return disjointTopOnly.connected(setIndex, topIndex);
     }
     public int numberOfOpenSites() {
         return sites;
@@ -94,31 +105,6 @@ public class Percolation {
     }
 
     public static void main(String[] args) {
-        // 0 0 0 0 0 1
-        // 1 1 1 1 0 1
-        // 1 0 0 1 0 1
-        // 1 0 0 1 0 1
-        // 1 1 0 1 1 1
-        // 0 1 0 0 0 0
-        Percolation test = new Percolation(6);
-        test.open(0, 5);
-        test.open(1, 5);
-        test.open(2, 5);
-        test.open(3, 5);
-        test.open(4, 5);
-        test.open(4, 4);
-        test.open(3, 3);
-        test.open(2, 3);
-        test.open(1, 3);
-        test.open(1, 2);
-        test.open(1, 1);
-        test.open(1, 0);
-        test.open(2, 0);
-        test.open(3, 0);
-        test.open(4, 0);
-        test.open(4, 1);
-        test.open(5, 1);
-        test.open(4, 3);
-        System.out.println(test.percolates());
+
     }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -6,8 +6,6 @@ import edu.princeton.cs.introcs.StdStats;
 public class PercolationStats {
     private int lengthOfGrid;
     private int numberOfExperiments;
-    private double mean;
-    private double dev;
     private PercolationFactory sampleMaker;
     private double[] expThreshold;
     public PercolationStats(int N, int T, PercolationFactory pf) {
@@ -18,36 +16,32 @@ public class PercolationStats {
         numberOfExperiments = T;
         sampleMaker = pf;
         expThreshold = new double[numberOfExperiments];
-        int currExperiment = 0;
-        while (currExperiment < numberOfExperiments) {
+        for (int i = 0; i < N; i++) {
             Percolation sample = sampleMaker.make(lengthOfGrid);
-            int result = 0;
-            while (!sample.percolates()) {
-                int row = StdRandom.uniform(lengthOfGrid);
-                int col = StdRandom.uniform(lengthOfGrid);
-                sample.open(row, col);
-                result++;
-            }
-            expThreshold[currExperiment] = (((double) result) / (lengthOfGrid * lengthOfGrid));
-            currExperiment++;
+            expThreshold[i] = doExperiment(sample, lengthOfGrid);
         }
-
+    }
+    private double doExperiment(Percolation p, int N) {
+        while (!p.percolates()) {
+            int row = StdRandom.uniform(0, N);
+            int col = StdRandom.uniform(0, N);
+            p.open(row, col);
+        }
+        return (double) p.numberOfOpenSites() / (N * N);
     }
     public double mean() {
-        mean = StdStats.mean(expThreshold);
-        return mean;
+        return StdStats.mean(expThreshold);
     }
     public double stddev() {
-        dev = StdStats.stddev(expThreshold);
-        return dev;
+        return StdStats.stddev(expThreshold);
     }
     public double confidenceLow() {
-        double low = mean - (1.96 * dev) / java.lang.Math.sqrt(numberOfExperiments);
-        return low;
+        double value = (1.96 * stddev()) / java.lang.Math.sqrt(numberOfExperiments);
+        return mean() - value;
     }
     public double confidenceHigh() {
-        double high = mean + (1.96 * dev) / java.lang.Math.sqrt(numberOfExperiments);
-        return high;
+        double value = (1.96 * stddev()) / java.lang.Math.sqrt(numberOfExperiments);
+        return mean() + value;
     }
 
 }
