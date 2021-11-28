c8e6122e38e4c31afb8efa4969bf173d13ec3464
gavingoh99 <gavingoh99@gmail.com>
2021-11-28 17:36:22 +0800

    proj3 phase 1

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -42,11 +42,115 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+
+        // params: lrlon, ullon, w, h, ullat, lrlat
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        double ullat = params.get("ullat");
+        if (params.get("ullat") > MapServer.ROOT_ULLAT) {
+            ullat = MapServer.ROOT_ULLAT;
+        }
+        double ullon = params.get("ullon");
+        if (params.get("ullon") < MapServer.ROOT_ULLON) {
+            ullat = MapServer.ROOT_ULLON;
+        }
+        double lrlat = params.get("lrlat");
+        if (params.get("lrlat") < MapServer.ROOT_LRLAT) {
+            lrlat = MapServer.ROOT_LRLAT;
+        }
+        double lrlon = params.get("lrlon");
+        if (params.get("lrlon") > MapServer.ROOT_LRLON) {
+            lrlon = MapServer.ROOT_LRLON;
+        }
+        // check if ullon and ullat are within the root lon and lat
+        if (!validateParams(ullat, lrlat, ullon, lrlon)) {
+            results.put("render_grid", null);
+            results.put("roster_ul_lon", 0);
+            results.put("roster_ul_lat", 0);
+            results.put("roster_lr_lon", 0);
+            results.put("roster_lr_lat", 0);
+            results.put("depth", 0);
+            results.put("query_success", false);
+            return results;
+        }
+        // calculate user LDPP
+        double userLDPP = (lrlon - ullon) / params.get("w");
+        // compare with LDPP of tiles
+        // ldpp of 0th level of zoom
+        double LDPP = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / 256;
+        int depth = 0;
+        boolean foundRightTiles = false;
+        while (!foundRightTiles) {
+            // londpp of tile must be less than or equal to user
+            // else if user londpp lower than depth 7
+            // use depth 7 as the default
+            if (LDPP <= userLDPP || depth == 7) {
+                foundRightTiles = true;
+                break;
+            }
+            // ldpp of next level of zoom;
+            LDPP = LDPP / 2;
+            depth++;
+        }
+        // find the interval dist,
+        // depth 0 has 1 x 1 grid, 1 has 2 x 2 grid, 2 has 4 x 4 grid
+        // number of grids per axis is 2^depth
+
+        double lonIntervalDist = (MapServer.ROOT_ULLON - MapServer.ROOT_LRLON) / Math.pow(2, depth);
+        double latIntervalDist = (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / Math.pow(2, depth);
+
+        int startX = (int) ((MapServer.ROOT_ULLON - ullon) / lonIntervalDist);
+        int startY = (int) ((MapServer.ROOT_ULLAT - ullat) / latIntervalDist);
+        int endX = (int) ((MapServer.ROOT_ULLON - lrlon) / lonIntervalDist);
+        int endY = (int) ((MapServer.ROOT_ULLAT - lrlat) / latIntervalDist);
+
+        // corresponding size of 2d array
+        // y is number of vertical gridboxes
+        // x is number of horizontal gridboxes
+        // then iterate through each index of array
+        // placing corresponding filename as a string d0_x0_y0.png for example
+        int numRows = endY - startY + 1;
+        int numCols = endX - startX + 1;
+        String[][] grid = new String[numRows][numCols];
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                grid[row][col] = "d" + depth + "_x" + (col + startX) + "_y" + (row + startY) + ".png";
+            }
+        }
+
+        // corresponding ullon and ullat of first gridbox is
+        // the map ullon and ullat with the distance from gridbox
+        // to topleft gridbox of map
+        // corresponding lrlon and lrlat of last gridbox is
+        // the ullon and ullat of querybox with distance number of
+        // x and y intervals taken into account
+
+        // corresponding x and y gridbox is the distance from
+        // ullon and ullat of the map divided by interval distance
+        double rasterULLon = MapServer.ROOT_ULLON - lonIntervalDist * startX;
+        double rasterULLat = MapServer.ROOT_ULLAT - latIntervalDist * startY;
+        double rasterLRLon = rasterULLon - lonIntervalDist * numCols;
+        double rasterLRLat = rasterULLat - latIntervalDist * numRows;
+
+        results.put("render_grid", grid);
+        results.put("raster_ul_lon", rasterULLon);
+        results.put("raster_ul_lat", rasterULLat);
+        results.put("raster_lr_lon", rasterLRLon);
+        results.put("raster_lr_lat", rasterLRLat);
+        results.put("depth", depth);
+        results.put("query_success", true);
         return results;
     }
+    private boolean validateParams(double ullat, double lrlat, double ullon, double lrlon) {
+        //ullat = 37.89, lrlat = 37.82 -> to the left means ullat > lrlat
+        //ullon = -122.29, lrlon = -122.21 -> to the left means ullon < lrlon
 
+        //check that ullat, ullon are in range
+        //check that ullat and ullon are to the left of lrlat and lrlon
+        if (ullat >= MapServer.ROOT_LRLAT && ullat <= MapServer.ROOT_ULLAT) {
+            if (ullon >= MapServer.ROOT_ULLON && ullon <= MapServer.ROOT_LRLON) {
+                return ullat >= lrlat && ullon <= lrlon;
+            }
+        }
+        return false;
+    }
 }
